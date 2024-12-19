6fdf90cc7f758ebe65b9334b1550dacab3b4bb04
andrinSutter <148957523+andrinSutter@users.noreply.github.com>
2024-12-19 14:05:14 +0100

    changed role init.sh

--- src/init.sh
+++ src/init.sh
@@ -16,35 +16,15 @@ else
     aws s3api create-bucket --bucket "$OUT_BUCKET_NAME" --region "$AWS_REGION" --create-bucket-configuration LocationConstraint="$AWS_REGION"
 fi
 
-echo "Creating IAM Role for Lambda..."
-if ! aws iam get-role --role-name "$LAMBDA_ROLE_NAME" 2>/dev/null; then
-    aws iam create-role --role-name "$LAMBDA_ROLE_NAME" --assume-role-policy-document '{
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "lambda.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }'
-    aws iam attach-role-policy --role-name "$LAMBDA_ROLE_NAME" --policy-arn "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-    aws iam attach-role-policy --role-name "$LAMBDA_ROLE_NAME" --policy-arn "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-else
-    echo "IAM Role $LAMBDA_ROLE_NAME already exists."
-fi
-
 echo "Packaging Lambda function..."
 LAMBDA_ZIP="lambda_function.zip"
 zip -j "$LAMBDA_ZIP" lambda_function.py
 
 echo "Deploying Lambda function..."
-LAMBDA_ROLE_ARN=$(aws iam get-role --role-name "$LAMBDA_ROLE_NAME" --query 'Role.Arn' --output text)
+LAMBDA_ROLE_ARN=arn:aws:iam::066083534964:role/LabRole
 aws lambda create-function --function-name "$LAMBDA_FUNCTION_NAME" \
     --runtime python3.9 \
-    --role "$LAMBDA_ROLE_ARN" \
+    --role ${LAMBDA_ROLE_ARN} \
     --handler lambda_function.lambda_handler \
     --zip-file fileb://"$LAMBDA_ZIP" \
     --environment Variables="{OUT_BUCKET_NAME=$OUT_BUCKET_NAME}" \
